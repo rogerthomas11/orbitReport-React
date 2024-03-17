a8991958d0ed1d9cfb78a29c765cba407c663ad3
Roger Thomas <rogerthomas96@outlook.com>
2024-03-16 22:58:42 -0500

    complette through task 4

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,11 +3,17 @@ const Table = ({ sat }) => {
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
      <tr>
+       <td>Row Data TBD</td>
+       <td>Row Data TBD</td>
+       <td>Row Data TBD</td>
        <td>Row Data TBD</td>
      </tr>
      </tbody>
